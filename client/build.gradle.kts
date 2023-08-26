cdaebbdd45a4c2e0e2e67b1a7ac94a6cce8cc402
Brady Svedin <brady@lightningkite.com>
2023-08-25 19:47:49 -0700

    Dependency updates

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 val kotlinVersion: String by project
-val rxPlusVersion = "1.0.5"
+val rxPlusVersion = "1.0.6"
 val khrysalisVersion: String by project
 dependencies {
     api(project(":shared"))
